d46461138be9c570c5e300637411104f419041f7
Carlos Precioso <github@precioso.design>
2024-10-04 19:44:24 +0200

    Add type exports to prelude

--- src/lib/swiftdoc-to-zod/zod-prelude.ts
+++ src/lib/swiftdoc-to-zod/zod-prelude.ts
@@ -1,11 +1,16 @@
 import * as z from "zod";
 
 export const iso8601 = z.date().transform((date) => date.toISOString());
+export type iso8601 = z.input<typeof iso8601>;
 
 export const iso4217 = z
   .string()
   .length(3)
   .transform((code) => code.toUpperCase());
+export type iso4217 = z.input<typeof iso4217>;
 
 export const localizableString = z.string();
+export type localizableString = z.input<typeof localizableString>;
+
 export const localizableFormatString = z.string();
+export type localizableFormatString = z.input<typeof localizableFormatString>;
