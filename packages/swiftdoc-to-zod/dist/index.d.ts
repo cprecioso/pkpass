6fd8f5b4ed7ac6a584e4c865adea621331fed2bc
Carlos Precioso <github@precioso.design>
2025-02-02 23:47:40 +0100

    Remove dist

--- packages/swiftdoc-to-zod/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const convertSchema: (doc: string, { baseUrl, baseUri, fetchJson, }: {
-    baseUrl: string;
-    baseUri: string;
-    fetchJson: (url: string) => Promise<unknown>;
-}) => Promise<string>;
-
-export { convertSchema };
